82fd8647c7b83e3626f8e816ade9b5f966dc5dff
Dharma Reddy Bojjireddy <v-dhbojj@microsoft.com>
2020-07-27 12:30:03 +0530

    basic changes

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request , jsonify;
+import psycopg2;
+import json;
+
+
+class employee:
+    def __init__(self, empid, empname):
+        self.empid = empid
+        self.empname = empname
+
+app = Flask(__name__);
+
+@app.route('/')
+def greet():
+    return "Hello python"
+
+@app.route('/api/v1/timesheet/employee/all',methods=["GET"])
+def hello():
+     employees = []
+     host = "postgresqldemo.postgres.database.azure.com"
+     user = "csvadmin@postgresqldemo"
+     dbname = "postgres"
+     password = "admin@123"
+     sslmode = "require"
+     conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(host, user, dbname, password, sslmode)
+     conn = psycopg2.connect(conn_string) 
+     cursor = conn.cursor()
+     cursor.execute("SELECT * FROM empdata")
+     rows = cursor.fetchall()
+     return jsonify(rows);
+            
+if __name__ == '__main__':
+    app.run('localhost',4449)
